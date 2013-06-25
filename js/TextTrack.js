4dce39f919c29f666cfe0deaa0a41116cc760667
Logan Kearsley <chronosurfer@gmail.com>
2013-06-24 19:09:19 -0600

    Improve efficiency of active cue updates.

--- js/TextTrack.js
+++ js/TextTrack.js
@@ -2,7 +2,6 @@
 
 var TextTrack = (function(){
 	var textKinds = {
-		// WHATWG SPEC
 		"subtitles":true,
 		"captions":true,
 		"descriptions":true,
@@ -228,34 +227,25 @@ TextTrackCueList.prototype.removeCue = function(cue) {
 };
 
 function ActiveTextTrackCueList(textTrackCueList,textTrack) {
-	// Among active cues:
-
-	// The text track cues of a media element's text tracks are ordered relative to each
-	// other in the text track cue order, which is determined as follows: first group the
-	// cues by their text track, with the groups being sorted in the same order as their
-	// text tracks appear in the media element's list of text tracks; then, within each
-	// group, cues must be sorted by their start time, earliest first; then, any cues with
-	// the same start time must be sorted by their end time, earliest first; and finally,
-	// any cues with identical end times must be sorted in the order they were created (so
-	// e.g. for cues from a WebVTT file, that would be the order in which the cues were
-	// listed in the file).
-
 	this.refreshCues = function() {
 		if (!textTrackCueList.length) { return; }
-		var cueList = this,
-			oldCueList = this.slice(0),
+		var i, j, cue,
 			cueListChanged = false;
 			
-		this.length = 0;
-		textTrackCueList.forEach(function(cue) {
+		for(i = 0, j = 0; cue = textTrackCueList[i]; i++){
 			if (cue.active) {
-				cueList.push(cue);
-				cueListChanged |= (cue !== oldCueList[cueList.length-1]);
+				cueListChanged |= cue !== this[j];
+				this[j++] = cue;
+			} else if(j > 0){
+				break;
 			}
-		});
+		}
 
-		try { cueListChanged && textTrack.oncuechange(); }
-		catch(error) {}
+		this.length = j;
+		
+		if(cueListChanged && typeof textTrack.oncuechange === 'function'){
+			textTrack.oncuechange();
+		}
 	};	
 	this.refreshCues();
 };
